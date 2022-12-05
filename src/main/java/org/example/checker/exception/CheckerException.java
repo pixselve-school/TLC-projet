b59dd251cc60b7cd49778e867fa67eeb39bda2c1
Fgdou <fabigoardou@gmail.com>
2022-12-05 11:31:41 +0100

    UPDATE checker -> beautify errors

--- src/main/java/org/example/checker/exception/CheckerException.java
+++ src/main/java/org/example/checker/exception/CheckerException.java
@@ -3,10 +3,47 @@ package org.example.checker.exception;
 import org.antlr.runtime.tree.CommonTree;
 
 public abstract class CheckerException extends Exception {
-    public CheckerException(String filename, String message, int line, int cpos) {
-        super(filename + ":" + line + ":" + cpos + " " + message);
-    }
+
+    String filename;
+    int line;
+    int charPos;
+    String message;
+
     public CheckerException(String filename, CommonTree tree, String message){
-        super(filename + ":" + tree.getLine() + ":" + tree.getCharPositionInLine() + " " + message);
+        super("\n" +
+                "\t" + filename + ":" + tree.getLine() + ":" + tree.getCharPositionInLine() + "\n" +
+                "\t" + message + "\n"
+        );
+        this.filename = filename;
+        line = tree.getLine();
+        charPos = tree.getCharPositionInLine();
+        this.message = message;
+    }
+
+    public String toString(String line){
+        StringBuilder res = new StringBuilder();
+
+        String spaces = "-".repeat(charPos) + '^';
+
+        res.append(message);
+        res.append('\n');
+        res.append("\tat ").append(filename).append(':').append(this.line).append(':').append(charPos).append('\n');
+        res.append('\t').append(line).append('\n');
+        res.append('\t').append(spaces);
+        res.append("\n\n");
+
+        return res.toString();
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public int getLine() {
+        return line;
+    }
+
+    public int getCharPos() {
+        return charPos;
     }
 }
