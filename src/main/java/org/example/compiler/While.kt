b85b9572ef09b126f638cfe75dfdeb8dcc4ae5e0
Mael Kerichard <38262536+Pixselve@users.noreply.github.com>
2023-01-02 12:14:18 +0100

    feat: while loop

--- src/main/java/org/example/compiler/While.kt
+++ src/main/java/org/example/compiler/While.kt
@@ -14,12 +14,23 @@ import org.antlr.runtime.tree.Tree
  * └────────┘
  */
 object While {
-    fun toCode(result: List<String>, tree: Tree) {
+
+    fun toCode(result: MutableList<String>, tree: Tree, index: String) {
         val expression = tree.getChild(0)
         val commands = tree.getChild(1)
         // generate code for expression
         val expressionClass = Expression(expression)
         val expressionResult = expressionClass.toCode()
-
+        // generate code for commands
+        val codeInsideCommands = Compiler.compile(commands)
+        // generate code for while
+        result.addAll(expressionResult.prepend)
+        result.add("param ${expressionResult.value}")
+        result.add("WHILE_BOOL_$index = call convertToBoolean 1")
+        result.add("WHILE_$index:")
+        result.addAll(codeInsideCommands)
+        result.add("param ${expressionResult.value}")
+        result.add("WHILE_BOOL_$index = call convertToBoolean 1")
+        result.add("if WHILE_BOOL_$index goto WHILE_$index")
     }
 }
