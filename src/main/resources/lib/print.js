e11ceb65d5baa2f7652b7be260ecbb172fc2fbd3
fgdou <fabigoardou@gmail.com>
2023-01-17 12:54:03 +0100

    fix: pretty printing

--- src/main/resources/lib/print.js
+++ src/main/resources/lib/print.js
@@ -1,10 +1,20 @@
 function print(t){
-    console.log(toInt(t))
-    console.log(toBool(t))
-    console.log(toString(t))
-    printTree(t, "", true)
 
-    console.log(t)
+    if(Array.isArray(t) && t.length >= 1){
+        let type = t[0]
+        if(type === "int")
+            console.log(toInt(t[1]))
+        else if(type === "bool")
+            console.log(toBool(t[1]))
+        else if(type === "string")
+            console.log(toString(t[1]))
+        else
+            printTree(t, "", true)
+    }else{
+        printTree(t, "", true)
+    }
+
+    // console.log(t)
 }
 function printTree(t, tab = "", last = false){
     let tabc = tab + ((last) ? "└─" : "├─")
@@ -16,9 +26,9 @@ function printTree(t, tab = "", last = false){
 
         for(let i in t) {
             if(last)
-                printTree(t[i], tab + "  ", i == t.length-1)
+                printTree(t[i], tab + "  ", i == t.length-1) // Do not change the ==
             else
-                printTree(t[i], tab + "│ ", i == t.length-1)
+                printTree(t[i], tab + "│ ", i == t.length-1) // Do not change the ==
         }
     }else
         console.log(tab + t)
